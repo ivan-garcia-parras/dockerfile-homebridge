f0794808b9eeda16b951ad1bfab1cabbf818ccfd
ivan-garcia-parras <ivangp@gmail.com>
2024-07-02 11:35:00 +0200

    npm install github:ivan-garcia-parras/homebridge-daikin-onecta#v1.0.0

--- startup.sh
+++ startup.sh
@@ -5,4 +5,4 @@ npm install github:ivan-garcia-parras/homebridge-eufy-legacy#v4.1.0
 npm install homebridge-webos-tv@v2.4.4
 npm install homebridge-z2m@v1.10.0
 npm install @vectronic/homebridge-script-switch@0.1.1
-npm install homebridge-daikin-cloud@2.0.0-beta.0
+npm install github:ivan-garcia-parras/homebridge-daikin-onecta#v1.0.0
