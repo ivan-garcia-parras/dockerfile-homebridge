628af95abcd91d2eb344fdd53e719df0b83e51a3
ivan-garcia-parras <ivangp@gmail.com>
2024-07-17 08:50:53 +0200

    Update homebridge-lg-thinq. Migrate to homebridge-eufy-security

--- startup.sh
+++ startup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-npm install homebridge-lg-thinq@v1.8.4
-npm install github:ivan-garcia-parras/homebridge-eufy-legacy#v4.1.0
+npm install homebridge-lg-thinq@v1.8.7
+npm install homebridge-eufy-security@3.4.2
 npm install homebridge-webos-tv@v2.4.4
 npm install homebridge-z2m@v1.10.0
 npm install @vectronic/homebridge-script-switch@0.1.1
